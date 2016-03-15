ebc342a67811f2033bed411e84ea781d7934aa87
ffredyk <ffredyk@centrum.cz>
2016-03-15 11:07:55 +0100

    Added language files
    CZ almost completed
    EN,DE prepared to translation

---
+++ lang/DE.php
@@ -0,0 +1,110 @@
+<?php
+
+//TODO: TRANSLATE TO DE
+
+$lang_Header = array(
+  "PageTitle" => "Arma Events",
+  "PageDesc" => "Arma 3 events international community for all players",
+  "Navigation" => array(
+    "MainPage" => "Hlavní stránka",
+    "Community" => "Komunita",
+    "Play" => "Hrát",
+    "AddonList" => "Seznam addonù",
+    "Briefing" => "Briefing",
+    "User" => array(
+      "EventCreate" => "Vytvoøit novı event",
+      "EventList" => "Seznam mıch eventù",
+      "Stats" => "Herní statistika",
+      "Settings" => "Nastavení",
+      "UserProfile" => "Uivatelskı profil",
+      "Logout" => "Odhlásit se"
+    )
+  )
+);
+
+$lang_Page = array(
+  "WIPTitle" => "Stále ve vırobì",
+  "WIP" => "Ještì to tu není plnì pøipravené a hotové. Take zatím vydrte a mìjte strpení",
+  "AEHeaderTitle" => "Arma Events",
+  "AEHeader" => "Vítejte na novém portálu Arma Events! Jste na správném místì pokud hledáte trochu zábavy",
+  "AEWelcome" => "Vítejte na Arma Events!",
+  "ActualEvent" => array(
+    "Title" => "Aktuální event",
+    "Open" => "Otevøít informace o eventu"
+  ),
+  "News" => array(
+    "Title" => "Poslední novinka",
+    "Open" => "Otevøít novinku"
+  ),
+  "ServerInfo" => array(
+    "Title" => "Informace o serveru",
+    "IP" => "IP serveru:",
+    "Hostname" => "Hostname:",
+    "Players" => "Hráèi:",
+    "Mission" => "Mise:",  
+    "BtnConnect" => "Pøipojit se na server",
+    "BtnInfo" => "Seznam hráèù"
+  ),
+  "Mainpage" => array(
+    "LastPictures" => "Obrázky z posledních eventù",
+    "Rules" => array(
+      "Title" => "Pravidla herního portálu a serveru",
+      "Section0" => "Pokud s náma chcete hrát, musíte dodrovat pravidla slušného chování! Kdokoliv pravidla poruší, u si s náma nezahraje.",         
+      "Section1Title" => "Herní server",
+      "Section1" => "UNDEFINED",
+      "Section2Title" => "Všeobecnì",
+      "Section2" => "UNDEFINED"
+    )
+  ),
+  "Community" => array(
+    "Title" => "Komunita Arma Events",
+    "NewsColumn" => array(
+      "Title" => "Poslední novinka"
+    ),
+    "ShoutboxColumn" => array(
+      "Title" => "Shoutbox",
+      "Message" => "Zpráva",
+      "MessageDesc" => "Zpráva pro ostatní",
+      "BtnSend" => "Pošli!",
+      "Smileys" => "Smajly",
+      "Admin" => "Admin"
+    ),
+    "RSSColumn" => array(
+      "Title" => "Komunitní novinky",
+      "InfoTitle  " => "RSS kanály",
+      "Info" => "Chcete aby váš kanál novinek RSS byl zvìøejnìn v tomto panelu? Napište nám!"
+    ),
+    "YTColumn" => array(
+      "Title" => "Komunitní video"
+    )
+  ),
+  "Play" => array(
+    "Title" => "Hrát s Arma Events",
+    "NeedsColumn" => array(
+      "Title" => "Co je potøeba",
+      "AddonsTitle" => "Addony", //TODO: localize addons tab
+      "ConnectionTitle" => "Pøipojení", //TODO: localize connection tab
+      "TSTitle" => "Teamspeak" //TODO: localize TS tab
+    ),
+    "EventsColumn" => array(
+      "Title" => "Uivatelské eventy",
+      "CreateTitle" => "Zalote vlastní událost",
+      "Create" => "Pøes uivatelské menu mùete zakládat vlastní eventy! Nejaktuálnìjší eventy se zobrazí zde v tomto seznamu!",
+      "ShowDone" => "Zobrazit všechny odehrané eventy"
+    )
+  ),
+  "Addons" => array(
+    "Title" => "Seznam addonù Arma Events",
+    "Info" => array(
+      "Title" => "Informace",
+      "Text" => "Náš server má vypnutou kontrolu klíèù, to znamená, e se na nìj pøipojíte i bez zaplıch addonù. To ovšem mùe zpùsobit nìjaké problémy pøi hraní, take neruèíme za funkènost hry pøi hraní bez potøebnıch addonù"
+    ),
+    "MainAddons" => array(
+      "Title" => "Hlavní addony",
+      "Disabled" => "Zatmavené addony nejsou na serveru právì k dispozici"
+    ),
+    "ClientAddons" => array(
+      "Title" => "Clientside addony"
+    )
+  )
+);
